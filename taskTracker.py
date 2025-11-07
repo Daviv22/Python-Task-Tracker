19a7ce0960c17b4dbf6b83e2577ea37e6092e77e
Davi Vieira Vitorino <davivvitorino@gmail.com>
2025-11-06 23:28:58 -0300

    Added basic main interface

--- taskTracker.py
+++ taskTracker.py
@@ -1,3 +1,6 @@
+import json
+import datetime
+
 def add_task():
     pass
 
@@ -10,9 +13,47 @@ def delete_task():
 def list_tasks():
     pass
 
-def mark_test():
+def mark_task():
+    pass
+
+def json_file():
+    tasksList = []
+
+    taskStructure = {
+        "id": "",
+        "description": "",
+        "status": "",
+        "createdAt": "",
+        "updatedAt": ""
+    }
+
+def check_decision(decisao):
     pass
 
 def run_app():
-    pass
+    while True:
+        print("O que você gostaria de fazer: ")
+        print("\t1 - Adicionar tarefa")
+        print("\t2 - Atualizar tarefa")
+        print("\t3 - Deletar tarefa")
+        print("\t4 - Marcar tarefa como ...")
+        print("\t5 - Listar tarefas ...")
+        decisao = input("Digite: ")
+        check_decision(decisao)
+
+run_app()
+
+"""
+    tasksLists = []
+
+    taskStructure = {
+        "id": "",
+        "description": "",
+        "status": "",
+        "createdAt": "",
+        "updatedAt": ""
+    }
+    with open("teste.json", "a") as f:
+        json.dump(tasksLists, f)
+"""
 
